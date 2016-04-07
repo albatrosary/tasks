aa29c92ed1f4b6314373ac3404ba203317ae5187
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-04-07 13:21:32 +0900

    install typings, typescript

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tasks
+
+## reference
+
+https://github.com/mgechev/angular2-style-guide
+
+https://www.npmjs.com/package/gulp-typescript
+
+```
+$ npm install typings, typescript -g
+$ typings init
+$ tsc --init
+```
---
+++ gulpfile.ts
@@ -0,0 +1,10 @@
+/// <reference path="./typings/main.d.ts"/>
+
+import * as gulp from 'gulp';
+const gulpLoadPlugins = require('gulp-load-plugins');
+const requireDir = require('require-dir');
+
+gulpLoadPlugins();
+requireDir('./tasks',{recurse:true});
+
+gulp.task('test', (done: any) => console.log('hoge'))
